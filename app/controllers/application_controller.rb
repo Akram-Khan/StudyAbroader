fbea63e2b0aebcfa6a48ed128aa55a4c211ea26e
Akram Khan <akramkhan83@gmail.com>
2012-03-26 18:53:31 +0530

    pushing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
 	protect_from_forgery
+	before_filter :authenticate
 
 	def after_sign_in_path_for(resource)
 		stored_location_for(resource) || user_home_user_path(current_user)
